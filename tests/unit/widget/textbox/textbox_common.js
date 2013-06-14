1a6d7ce4a3fd1aa8af7a7c98786e1105fa9f0460
eVo <evo2mind@gmail.com>
2013-06-14 14:21:08 +0700

    add uxTextbox testunit to test defined default options

---
+++ tests/unit/widget/textbox/textbox_common.js
@@ -0,0 +1,17 @@
+
+TestHelpers.commonWidgetTests( "uxTextbox", {
+    defaults: {
+        disabled: false,
+        create: null,
+        inlineLabel: '',
+        errorMessage: '',
+        inputMessage: '',
+        validatedMessage: 'validated',
+        maxChars: 80,
+        isRequired: false,
+        isReadonly: false,
+        isDisabled: false,
+        inputFilter: null,
+        inputFormat: null
+    }
+});
